4d0b426214803f56a7298e1422fb643b7d455594
UltraMango2319 <claudiohernaranjo@gmail.com>
2024-10-11 13:24:04 -0300

    Agregué Texto encima de la imagen del opening, todavia pendiente arreglar

--- app/src/main/java/com/example/proyectodeejemplo/MainActivity.java
+++ app/src/main/java/com/example/proyectodeejemplo/MainActivity.java
@@ -40,6 +40,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        binding.BTNCL2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Openingscrn.class);
+                startActivity(intent);
+            }
+        });
+
 
 
 
