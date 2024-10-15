a880fa43441c8db3d197a0071aa8d929efc6a7f0
UltraMango2319 <claudiohernaranjo@gmail.com>
2024-10-14 22:44:30 -0300

    Opening con texto, metodos de modo oscuro agregados

--- app/src/main/java/com/example/proyectodeejemplo/Openingscrn.java
+++ app/src/main/java/com/example/proyectodeejemplo/Openingscrn.java
@@ -1,21 +1,37 @@
 package com.example.proyectodeejemplo;
 
+import android.graphics.Color;
 import android.os.Bundle;
-
+import android.content.res.Configuration;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.proyectodeejemplo.databinding.OpeningscrnBinding;
 
 public class Openingscrn extends AppCompatActivity {
 
     OpeningscrnBinding binding;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = OpeningscrnBinding.inflate(getLayoutInflater());
-
         setContentView(binding.getRoot());
+
+        int nightmodeflags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        switch(nightmodeflags){
+            case Configuration.UI_MODE_NIGHT_YES:
+                //el codigo que ocurrira si esta en night mode
+                binding.nombreInput.setHintTextColor(Color.BLACK);
+                binding.nombreInput.setTextColor(Color.BLACK);
+                break;
+            case Configuration.UI_MODE_NIGHT_NO:
+                //el codigo que ocurrira si no esta en night mode
+                binding.nombreInput.setHintTextColor(Color.BLACK);
+                binding.nombreInput.setTextColor(Color.BLACK);
+                break;
+        }
     }
+
+
 }
