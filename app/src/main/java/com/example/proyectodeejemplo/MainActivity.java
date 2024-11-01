ee308b18e527ae901e4a6942ef407a2de2f98987
Tide <tami.che@live.com>
2024-11-01 20:11:40 -0300

    Mal intento de hacer barra de navegación el error está en la main, ok

--- app/src/main/java/com/example/proyectodeejemplo/MainActivity.java
+++ app/src/main/java/com/example/proyectodeejemplo/MainActivity.java
@@ -1,61 +1,67 @@
 package com.example.proyectodeejemplo;
 
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
-import android.widget.RadioGroup;
-import android.widget.RatingBar;
-import android.widget.Toast;
-//
 import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.proyectodeejemplo.databinding.ActivityMainBinding;
+import com.example.proyectodeejemplo.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    //Instancias de los fragmentos para las secciones de las notas, checklist y el calendario
+    AgregarNotaFragment agregarNotaFragment = new AgregarNotaFragment();
+    CheckListYIPPIEEEFragment checkListYIPPIEEEFragment = new CheckListYIPPIEEEFragment();
+    VerNotasFragment verNotasFragment = new VerNotasFragment();
 
-    //Declaración de la variable binding que contendrá la referencia a la clase generada para el archivo XML de diseño
     ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Inicialización del binding creando una instancia del diseño XML asociado con la actividad
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-        //Establece el contenido de la actividad usando la raíz del diseño instanciado con binding
         setContentView(binding.getRoot());
 
+        //configuración del BottomNavigationView para gestionar la navegación entre fragmentos
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+        navigation.setOnItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        binding.BTNCL.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CheckListYIPPIEEE.class);
-                //un Intent es un objeto que se utiliza para enviar un mensaje desde una parte de la aplicación a otra. Es una forma de comunicar diferentes componentes de la aplicación
-                startActivity(intent);
-            }
-        });
-        binding.botonVerNotas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, VerNotas.class);
-                startActivity(intent);
-            }
-        });
-        binding.botonCrearNotas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AgregarNotas.class);
-                startActivity(intent);
-            }
-        });
+        //acá se carga el primer fragmento al momento de iniciar la actividad ¿
+        loadFragment(agregarNotaFragment);
+    }
 
+    //Listener para manejar las selecciones en el BottomNavigationView
+    private final BottomNavigationView.OnItemSelectedListener mOnNavigationItemSelectedListener =
+            new BottomNavigationView.OnItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        //Llamen a dios
+                        case R.id.notas:
+                            loadFragment(agregarNotaFragment);
+                            return true;
+                        case R.id.checklist:
+                            loadFragment(checkListYIPPIEEEFragment);
+                            return true;
+                            //acá puse otra cosa pq no está el calendario aún pero dsps se cambia, aja
+                        case R.id.calendario:
+                            loadFragment(verNotasFragment);
+                            return true;
+                    }
+                    return false;
+                }
+            };
 
-
-
+    //Método para cargar y mostrar el fragmento en el contenedor de la actividad
+    public void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment); //Reemplaza el fragmento actual por el nuevo
+        transaction.commit(); //Confirma la transacción de fragmento
     }
 }
