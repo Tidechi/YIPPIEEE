74e244ec465e9df76db99b0c4516657bd8fc14ad
UltraMango2319 <claudiohernaranjo@gmail.com>
2024-11-14 02:17:22 -0300

    Colores originales restaurados

--- app/src/main/java/com/example/proyectodeejemplo/CheckListYIPPIEEEFragment.java
+++ app/src/main/java/com/example/proyectodeejemplo/CheckListYIPPIEEEFragment.java
@@ -1,7 +1,10 @@
 package com.example.proyectodeejemplo;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,12 +18,14 @@ import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.proyectodeejemplo.databinding.ActivityCheckListYippieeeBinding;
+import com.google.android.material.color.DynamicColors;
 
 import java.util.Calendar;
 
@@ -37,6 +42,7 @@ public class CheckListYIPPIEEEFragment extends Fragment implements TaskManager.C
     private ImageView YIPPIEEE;
     private GifImageView confetti;
     public String fechaDeHoy;
+    private DatePickerDialog datePickerDialog;
 
     @Nullable
     @Override
@@ -63,6 +69,14 @@ public class CheckListYIPPIEEEFragment extends Fragment implements TaskManager.C
             }
         });
 
+        binding.fechaChecklist.setOnClickListener(view -> {
+            if (isAdded()) {
+                showDatePicker(view);
+            } else {
+                Log.e("AgregarNotaFragment", "Fragment not attached to activity");
+            }
+        });
+
         return v;
     }
 
@@ -79,6 +93,34 @@ public class CheckListYIPPIEEEFragment extends Fragment implements TaskManager.C
         int month = cal.get(Calendar.MONTH) +1;
         int day = cal.get(Calendar.DAY_OF_MONTH);
         return  makeDateString(day, month, year);
+    }
+
+    private void initDatePicker() {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+                binding.fechaChecklist.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(requireContext(), style, dateSetListener, year, month, day);
+    }
+
+    public void showDatePicker(View view) {
+        if (datePickerDialog != null) {
+            datePickerDialog.show();
+        } else {
+            Log.e("CheckListYIPPIEEEFragment", "DatePickerDialog is not initialized");
+        }
     }
 
     private String makeDateString(int day, int month, int year) {
